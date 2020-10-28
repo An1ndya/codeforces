e5d25ae055de75d1af2a957784c3e056e85d8874
Anindya Biswas <anindya33046@gmail.com>
2020-10-28 18:59:37 +0600

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -3,16 +3,11 @@
 using namespace std;
  
  
-int  n,m ;
-int minstep=1000000000;
-//void DFS(int nodevalue,int stepcount);
 
  
 main()
 {
  
-	cin>>n>>m;
-    //reverseDFS(n,0);
-    cout<<minstep;
+
 	return 0;
 }
