816a387b5d8b081de1dd69fe9e002ffa2af3a206
Anindya Biswas <anindya33046@gmail.com>
2020-10-28 18:47:44 +0600

    Create test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ 
+int  n,m ;
+int minstep=1000000000;
+//void DFS(int nodevalue,int stepcount);
+
+ 
+main()
+{
+ 
+	cin>>n>>m;
+    //reverseDFS(n,0);
+    cout<<minstep;
+	return 0;
+}
